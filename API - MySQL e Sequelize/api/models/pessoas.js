8490e8eab268570d0d7e238af04ca54285b625e7
Laisson Bruno <laissontkd@hotmail.com>
2023-08-15 18:33:51 -0300

    MySQL e Sequelize 2.0

--- API - MySQL e Sequelize/api/models/pessoas.js
+++ API - MySQL e Sequelize/api/models/pessoas.js
@@ -5,18 +5,52 @@ const {
 module.exports = (sequelize, DataTypes) => {
   class Pessoas extends Model {
     static associate(models) {
-      Pessoas.hasMany(models.Turmas, {foreignKey: 'docente_id'})
-      Pessoas.hasMany(models.Matriculas, {foreignKey: 'estudante_id'})
+      Pessoas.hasMany(
+        models.Turmas,
+        {
+          foreignKey: 'docente_id'
+        })
+      Pessoas.hasMany(
+        models.Matriculas,
+        {
+          foreignKey: 'estudante_id',
+          scope: { status: 'confirmado' },
+          as: 'aulasMatriculadas'
+        })
+
     }
   }
   Pessoas.init({
-    nome: DataTypes.STRING,
+    nome: {
+      type: DataTypes.STRING,
+      validate: {
+        funcaoValidadora: function (dado) {
+          if (dado.length < 3) throw new Error('O campo nome deve ter mais de três caracteres')
+        }
+      }
+    },
     ativo: DataTypes.BOOLEAN,
-    email: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: {
+          args: true,
+          msg: 'Email inválido'
+        }
+      }
+    },
     role: DataTypes.STRING
   }, {
     sequelize,
     modelName: 'Pessoas',
+    paranoid: true,
+    defaultScope: {
+      where: { ativo: true }
+    },
+    scopes: {
+      todos: { where: {} }
+      // etc: { constraint: valor }
+    }
   });
   return Pessoas;
 };
--- API - MySQL e Sequelize/api/models/turmas.js
+++ API - MySQL e Sequelize/api/models/turmas.js
@@ -15,6 +15,7 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'Turmas',
+    paranoid: true
   });
   return Turmas;
 };
